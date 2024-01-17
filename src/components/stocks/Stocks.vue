cb4fa849d34197e85b86568acf64a7de3f81e87c
Anderson Bauermann Feltes <anderson.bauermannfeltes@gmail.com>
2024-01-17 08:28:11 -0300

    implemented new components
    implemented component header, stock and stocks

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,27 @@
 <template>
-    <h1>Ações</h1>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id" :stock="stock"></Stock>
+    </v-layout>
 </template>
 
 <script>
-export default {
 
+import Stock from './Stock.vue';
+
+export default {    
+    components: {
+        Stock
+    },
+    data() {
+        return {
+            stocks: [
+                { id: 1, name: 'BMW', price: 110 },
+                { id: 2, name: 'APPLE', price: 300 },
+                { id: 3, name: 'GOOGLE', price: 270 },
+                { id: 4, name: 'AMAZON', price: 240 }
+            ]
+        }
+    }
 }
 </script>
 
