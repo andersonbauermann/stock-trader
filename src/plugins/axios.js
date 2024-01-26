ff8b3fa6465974aa2758f4f45011b4f6e67d2b5e
Anderson Bauermann Feltes <anderson.bauermannfeltes@gmail.com>
2024-01-25 21:24:29 -0300

    url base fix
    url base fix axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,12 +2,10 @@ import Vue from 'vue';
 import axios from 'axios';
 
 require('dotenv').config();
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL
 
 Vue.use({
     install(Vue) {
-        Vue.prototype.$http = axios.create({
-            baseURL: process.env.BASE_URL
-
-        })
+        Vue.prototype.$http = axios.create();
     }
-})
+});
